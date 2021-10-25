56ff381ce4615afb3bedfb4865bfed6085c9b638
AarnavSan <aarnavsangekar@gmail.com>
2021-10-25 16:20:16 +0530

    make it better

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const { returnUTC } = require('./DateCalculations/date');
 const datec = require('./DateCalculations/date');
 const unixc = require('./DateCalculations/unix');
 
-const errorobject = {error : "Invalid Date"};
-var port = process.env.PORT || 3000;
-var reg = /^\d+$/;
-
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.static(__dirname + '/static/stylesheets'));
 
@@ -19,70 +15,34 @@ app.get('/', (req, res) => {
 
 
 app.get('/api', (req,res) => {
-    console.log(req.params);
-    let unix, utc;
-
-    let utcdate = datec.returnUTCforNow();
-    utc = datec.returnUTC(utcdate,true);
-    // cDay,cMonth,cYear,cHours,cMinutes,cSeconds
-    let temp = [utcdate[2], utcdate[1], utcdate[0], utcdate[3], utcdate[4], utcdate[5]]
-    // year, monthIndex, day, hours, minutes, seconds, milliseconds
-    unix = unixc.toTimestamp(utc.substring(6,26));
-
-    let ans = {
-        unix: unix,
-        utc: utc
-    }
-    console.log(ans);   
+    let dateObject = new Date();
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString()});  
     res.json(ans);
 });
 
-app.get('/api/:date', (req,res) => {
-    asyncToCalc(req,res);
+app.get("/api/:date_string", (req, res) => {
+    let dateString = req.params.date_string;
+  
+    //A 4 digit number is a valid ISO-8601 for the beginning of that year
+    //5 digits or more must be a unix time, until we reach a year 10,000 problem
+    if (/\d{5,}/.test(dateString)) {
+      let dateInt = parseInt(dateString);
+      //Date regards numbers as unix timestamps, strings are processed differently
+      res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+    } else {
+      let dateObject = new Date(dateString);
+  
+      if (dateObject.toString() === "Invalid Date") {
+        res.json({ error: "Invalid Date" });
+      } else {
+        res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+      }
+    }
+  });
+
+  app.listen(port, () => {
+    console.log("Listening on :" + port);
 });
- 
-async function asyncToCalc(req,res){
-    console.log(req.params);
-    let date = req.params.date;
-    let unix,utc;
-    if(date.includes('-'))
-    {
-        let dateArr = [];
-        dateArr = date.split('-');
-        dateArr = dateArr.reverse();
-        if(dateArr.length != 3){
-            res.json(errorobject);
-        }
-        for(let i =0; i < 3; i++)
-        {
-            if(reg.test(dateArr[i]) == false)
-            {
-                res.json(errorobject);
-            }
-        }
-        dateArr.push(00);
-        dateArr.push(00);
-        dateArr.push(00);
-        dateArr.forEach((elem,index,arr) => {
-            arr[index] = parseInt(elem);
-        });
-        utc = datec.returnUTC(dateArr,false);
-        unix = unixc.toTimestamp(utc.substring(6,26));
-    }
-    else if(reg.test(date)){
-        unix = parseInt(date);
-        utc = unixc.toDateString(date,false);
-    }
-    else{
-        res.json(errorobject);
-    }
-    let ans = {
-        unix : unix,
-        utc : utc
-    }
-    console.log(ans);
-    res.json(ans);
-}
 
 app.listen(port, () => {
     console.log("Listening on :" + port);
